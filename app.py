2b1bca9ca960a762b62fc3da166ab3a8a8aac733
Raghul Siddharth <ragulsiddharth@AGLS-MLT-1051.local>
2026-02-18 09:57:59 +0530

    Initial Flask App

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, render_template
+import datetime
+import zoneinfo
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/status')
+def status():
+    return jsonify({
+        'status': 'running',
+        'message': 'Flask app is live on EC2!',
+        'timestamp': datetime.datetime.now(zoneinfo.ZoneInfo('Asia/Kolkata')).isoformat()
+    })
+
+@app.route('/api/hello')
+def hello():
+    return jsonify({'message': 'Hello from Flask on EC2!'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=False)
